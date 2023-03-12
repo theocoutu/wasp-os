dd52779031cdce94169eb5e87ceb1af68840f6d1
theocoutu <theodorec1998@gmail.com>
2023-03-11 19:54:04 -0600

    Update lis2dh.py

--- wasp/drivers/lis2dh.py
+++ wasp/drivers/lis2dh.py
@@ -1,72 +1,4 @@
-# Distributed with a free-will license.
-# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-# LIS2DHTR
-# This code is designed to work with the LIS2DHTR_I2CS I2C Mini Module available from ControlEverything.com.
-# https://www.controleverything.com/content/Accelorometer?sku=LIS2DHTR_I2CS#tabs-0-product_tabset-2
-
-import I2C
-
-import time
-
-# Get I2C bus
-bus = smbus.SMBus(1)
-
-# LIS2DHTR address, 0x19(24)
-# Select control register1, 0x20(32)
-# 0x27(39) Power ON mode, Data rate selection = 10 Hz
-# X, Y, Z-Axis enabled
-bus.write_byte_data(0x19, 0x20, 0x27)
-
-# LIS2DHTR address, 0x19(24)
-# Select control register4, 0x23(35)
-# 0x00(00) Continuous update, Full-scale selection = +/-2G
-bus.write_byte_data(0x19, 0x23, 0x00)
-
-time.sleep(0.5)
-
-# LIS2DHTR address, 0x19(24)
-# Read data back from 0x28(40), 2 bytes
-# X-Axis LSB, X-Axis MSB
-data0 = bus.read_byte_data(0x19, 0x28)
-data1 = bus.read_byte_data(0x19, 0x29)
-
-# Convert the data
-xAccl = data1 * 256 + data0
-if xAccl > 32767 :
-xAccl -= 65536
-
-# LIS2DHTR address, 0x19(24)
-# Read data back from 0x2A(42), 2 bytes
-# Y-Axis LSB, Y-Axis MSB
-data0 = bus.read_byte_data(0x19, 0x2A)
-data1 = bus.read_byte_data(0x19, 0x2B)
-
-# Convert the data
-yAccl = data1 * 256 + data0
-if yAccl > 32767 :
-yAccl -= 65536
-
-# LIS2DHTR address, 0x19(24)
-# Read data back from 0x2C(44), 2 bytes
-# Z-Axis LSB, Z-Axis MSB
-data0 = bus.read_byte_data(0x19, 0x2C)
-data1 = bus.read_byte_data(0x19, 0x2D)
-
-# Convert the data
-zAccl = data1 * 256 + data0
-if zAccl > 32767 :
-zAccl -= 65536
-
-
-
-
-
-
-
-
-
-# SPDX-License-Identifier: LGPL-3.0-or-later
-# Copyright (C) 2020 Daniel Thompson
+# 
 
 """LIS2DH driver
 ~~~~~~~~~~~~~~~~~
@@ -110,68 +42,40 @@ class LIS2DH:
     def write_reg(self, addr, val):
         self._i2c.writeto_mem(_I2CADDR, addr, bytes((val,)))
 
-    def read_hrs(self):
-        # TODO: Try fusing the read of H & L
+    def read_x(self):
+        # TODO: Try fusing the read of LSB & MSB
         xl = self.read_reg(_X_LSB_R)
-        xm = self.read_reg(_X_LSB_R)
+        xm = self.read_reg(_X_MSB_R)
 
-        yl = self.read_reg(_C0DATAL)
-        yh = self.read_reg(_C0DATAL)
+        x_accel = xm * 256 + xl
+        if x_accel > 32767 :
+            x = x_accel -= 65536
+        else:
+            x = x_accel
 
-        zl = self.read_reg(_C0DATAL)
-        zh = self.read_reg(_C0DATAL)
+        return x
 
-        return (m << 8) | ((h & 0x0f) << 4) | (l & 0x0f) | ((l & 0x30) << 12)
+    def read_y(self):
+        # TODO: Try fusing the read of LSB & MSB
+        yl = self.read_reg(_Y_LSB_R)
+        ym = self.read_reg(_Y_MSB_R)
 
-    def read_als(self):
-        # TODO: Try fusing the read of H & L
-        m = self.read_reg(_C1DATAM)
-        h = self.read_reg(_C1DATAH)
-        l = self.read_reg(_C1DATAL)
+        y_accel = ym * 256 + yl
+        if y_accel > 32767 :
+            y = y_accel -= 65536
+        else:
+            y = y_accel
 
-        return (m << 3) | ((h & 0x3f) << 11) | (l & 0x07)
+        return y
 
-    def set_gain(self, gain):
-        if gain > 64:
-            gain = 64
-        hgain = 0
-        while (1 << hgain) < gain:
-            hgain += 1
-        self.write_reg(_HGAIN, hgain << 2)
+    def read_z(self):
+        zl = self.read_reg(_Z_LSB_R)
+        zm = self.read_reg(_Z_MSB_R)
 
-    def set_drive(self, drive):
-        """
-        Set LED drive current
-        Parameters:
-            drive (int) LED drive current
-                0 = 12.5 mA
-                1 = 20   mA
-                2 = 30   mA
-                3 = 40   mA
-        """
-        en = self.read_reg(_ENABLE)
-        pd = self.read_reg(_PDRIVER)
-       
-        en = (en & ~_ENABLE_PDRIVE1 ) | ((drive & 2) << 2)
-        pd = (pd & ~_PDRIVER_PDRIVE0) | ((drive & 1) << 6)
+        z_accel = zm * 256 + zl
+        if z_accel > 32767 :
+            z = z_accel -= 65536
+        else:
+            z = z_accel
 
-        self.write_reg(_ENABLE, en)
-        self.write_reg(_PDRIVER, pd)
-
-    def set_hwt(self, t):
-        """
-        Set wait time between each conversion cycle
-        Parameters:
-            t (int) Wait time between each conversion cycle
-                0 = 800   ms
-                1 = 400   ms
-                2 = 200   ms
-                3 = 100   ms
-                4 =  75   ms
-                5 =  50   ms
-                6 =  12.5 ms
-                7 =   0   ms
-        """
-        en = self.read_reg(_ENABLE)
-        en = (en & ~_ENABLE_HWT) | (t << 4)
-        self.write_reg(_ENABLE, en)
+        return z
